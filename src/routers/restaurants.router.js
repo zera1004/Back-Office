cd1e6694ed60d7197494e134a21ebbbf3a603071
Parkcw00 <parkcw0325@gmail.com>
2025-01-09 19:42:33 +0900

    🎉 add: 업장등록, 업장 정보 수정 유효성 검사 파일 생성 및 로직 작성

--- src/routers/restaurants.router.js
+++ src/routers/restaurants.router.js
@@ -1,10 +1,13 @@
 import express from 'express';
 import restaurantsController from '../controllers/restaurants.controller.js';
+import { createRestaurantValidator } from './create-restaurant-validator.middleware.js';
+import { updateRestaurantValidator } from './update-restaurant-validator.middleware.js';
 
 const restaurantRouter = express.Router();
 
 restaurantRouter.post(
   '/owners/me/restaurants',
+  createRestaurantValidator,
   restaurantsController.postRestaurant,
 );
 
@@ -15,6 +18,7 @@ restaurantRouter.get(
 
 restaurantRouter.patch(
   '/owners/:ownerId/me/restaurants/:restaurantId',
+  updateRestaurantValidator,
   restaurantsController.updateRestaurant,
 );
 restaurantRouter.delete(
