e77af61e7f6bf51df902598574af8fe925a8d3b5
Parkcw00 <parkcw0325@gmail.com>
2025-01-09 16:55:11 +0900

    🎉 add: cart 장바구니 라우터 파일 추가

--- src/routers/carts.router.js
+++ src/routers/carts.router.js
@@ -1,1 +1,10 @@
-//
+import express from 'express';
+import { prisma } from '../utils/prisma/index.js';
+
+const cartRouter = express.Router();
+
+cartRouter.post('/users/me/carts/:id', async (req, res, next) => {});
+
+cartRouter.get('/users/me/carts/:id', async (req, res, next) => {});
+
+export default restaurantRouter;
